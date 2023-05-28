bed849b6bb85f612901d3852b56a3e403ded3a96
Neil Grogan <neil@grogan.org>
2023-05-28 23:15:37 +0100

    add emacs ver check 27 for xlsx opening

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -19,7 +19,7 @@
 (with-eval-after-load 'org
   (require 'org-protocol)
   (add-to-list 'org-modules 'org-protocol)
-  (unless (version< emacs-version "26.3")
+  (unless (version< emacs-version "27.1")
     (add-to-list 'org-file-apps '("\\.xlsx?\\'" . default))))
 
 
